c98f32c02d4649f53466fe10e6ecd8f6d64f8713
monoamine11231 <monoamine11231@gmail.com>
2024-05-06 19:23:32 +0200

    fix(graphics): APC success is parsed using regex and regex is being initialized as lazy static

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,96 +1,133 @@
-use std::io::{stdin, stdout, Read, Stdin, Stdout, Write};
+use base64::{engine::general_purpose::STANDARD, Engine};
 use regex::{Captures, Regex};
-
+use std::{
+    io::{stdin, stdout, Read, Stdin, Stdout, Write},
+    iter::Peekable,
+    ops::Deref,
+    slice::Chunks,
+    str,
+    sync::OnceLock,
+};
 
 /* Should be executed only after uncooking the terminal. This method expects the
  * terminal that a non-blocking and unbuffered read from stdin is possible */
 pub fn terminal_graphics_test_support() -> Result<(), String> {
     let mut handle1: Stdout = stdout();
     print!("\x1B_Gi=31,s=1,v=1,a=q,t=d,f=24;AAAA\x1B\\\x1B[c");
-    handle1.flush()
+    handle1
+        .flush()
         .map_err(|x| format!("Could not flush stdout: {}", x))?;
 
     let mut buf: Vec<u8> = Vec::new();
     let mut handle2: Stdin = stdin();
 
-    handle2.read_to_end(&mut buf)
+    handle2
+        .read_to_end(&mut buf)
         .map_err(|x| format!("Could not read from stdin: {}", x))?;
 
-    let ind: Option<usize> = buf.as_slice()
+    let ind: Option<usize> = buf
+        .as_slice()
         .windows(2)
         .position(|window| window == [b'O', b'K']);
 
     let res = match ind {
         Some(_) => Ok(()),
-        None => Err("Terminal does not support Kitty graphics protocol.".to_string())
+        None => Err("Terminal does not support Kitty graphics protocol.".to_string()),
     };
     res
 }
 
 pub fn terminal_graphics_apc_success() -> Result<(), String> {
-    let mut buf: Vec<u8> = Vec::new();
+    static REKITTYMSG: OnceLock<Result<Regex, String>> = OnceLock::new();
+    let rekittymsg: Regex = REKITTYMSG
+        .get_or_init(|| {
+            return Regex::new(";([a-zA-Z]*)\x1B\\\\")
+                .map_err(|x| format!("Could not compile regex expression: {}", x));
+        })
+        .clone()?;
 
     let mut handle: Stdin = stdin();
-    while buf.len() < 2 || buf.as_slice()[buf.len()-2..].to_vec()!=vec![b'\x1B', b'\\'] {
-        let mut tmp: [u8;1] = [0];
-        handle.read(&mut tmp)
-            .map_err(|x| format!("Could not read from stdin: {}",x))?;
-        buf.push(tmp[0]);
-    }
-
-    let ind: Option<usize> = buf.as_slice()
-        .windows(2)
-        .position(|window| window == [b'O', b'K']);
-        
-    let res = match ind {
-        Some(_) => Ok(()),
-        None => Err("Display or transfering of graphics to terminal has failed."
-                        .to_string()),
-    };
-    res
-}
-
-pub fn terminal_graphics_allocate_id() -> Result<usize, String> {
-    let mut handle1: Stdout = stdout();
-    let mut handle2: Stdin = stdin();
-    handle1.write("\x1B_Gf=24,I=1,t=d,v=1,s=1;AAAA\x1B\\".as_bytes())
-        .map_err(|x| format!("Could not write to stdout: {}",x))?;
-    handle1.flush()
-        .map_err(|x| format!("Could not flush stdout: {}",x))?;
-
-    let rekittymsg: Regex = Regex::new("i=(\\d+).*;([a-zA-Z]*)\x1B\\\\")
-        .map_err(|x| format!("Could not compile regex expression: {}",x))?;
-    
     let mut reply: String = String::new();
-    handle2.read_to_string(&mut reply)
-        .map_err(|x| format!("Could not read from stdin: {}",x))?;
-    let reply_captures: Captures<'_> = rekittymsg.captures(reply.as_str())
+    handle
+        .read_to_string(&mut reply)
+        .map_err(|x| format!("Could not read from stdin: {}", x))?;
+    let reply_captures: Captures<'_> = rekittymsg
+        .captures(reply.as_str())
         .ok_or("Could not parse graphics responce from terminal".to_string())?;
-
-    let reply_status: &str = reply_captures.get(2)
+    let reply_status: &str = reply_captures
+        .get(1)
         .ok_or("Terminal replied with invalid responce to graphics query".to_string())?
         .as_str();
 
     if reply_status != "OK" {
-        Err(format!("Failed when trying to allocate graphics ID: {}",reply_status))?;
+        Err(format!(
+            "Failed when trying to allocate graphics ID: {}",
+            reply_status
+        ))?;
+    }
+    
+    Ok(())
+}
+
+pub fn terminal_graphics_allocate_id() -> Result<usize, String> {
+    /* The new lazy static */
+    static REKITTYMSG: OnceLock<Result<Regex, String>> = OnceLock::new();
+    let rekittymsg: Regex = REKITTYMSG
+        .get_or_init(|| {
+            return Regex::new("i=(\\d+).*;([a-zA-Z]*)\x1B\\\\")
+                .map_err(|x| format!("Could not compile regex expression: {}", x));
+        })
+        .clone()?;
+
+    let mut handle1: Stdout = stdout();
+    let mut handle2: Stdin = stdin();
+    handle1
+        .write("\x1B_Gf=24,I=1,t=d,v=1,s=1;AAAA\x1B\\".as_bytes())
+        .map_err(|x| format!("Could not write to stdout: {}", x))?;
+    handle1
+        .flush()
+        .map_err(|x| format!("Could not flush stdout: {}", x))?;
+
+    let mut reply: String = String::new();
+    handle2
+        .read_to_string(&mut reply)
+        .map_err(|x| format!("Could not read from stdin: {}", x))?;
+    let reply_captures: Captures<'_> = rekittymsg
+        .captures(reply.as_str())
+        .ok_or("Could not parse graphics responce from terminal".to_string())?;
+
+    let reply_status: &str = reply_captures
+        .get(2)
+        .ok_or("Terminal replied with invalid responce to graphics query".to_string())?
+        .as_str();
+
+    if reply_status != "OK" {
+        Err(format!(
+            "Failed when trying to allocate graphics ID: {}",
+            reply_status
+        ))?;
     }
 
-    let reply_id: usize = reply_captures.get(1)
+    let reply_id: usize = reply_captures
+        .get(1)
         .ok_or("Terminal replied with invalid responce to graphics query".to_string())?
         .as_str()
         .parse::<usize>()
-        .map_err(|x| format!("Could not parse terminal generated graphics ID: {}",x))?;
-    
+        .map_err(|x| {
+            return format!("Could not parse terminal generated graphics ID: {}", x);
+        })?;
+
     Ok(reply_id)
 }
 
 pub fn terminal_graphics_deallocate_id(id: usize) -> Result<(), String> {
     let mut handle: Stdout = stdout();
     handle
-        .write(format!("\x1B_Ga=d,d=I,i={};\x1B\\",id).as_bytes())
+        .write(format!("\x1B_Ga=d,d=I,i={};\x1B\\", id).as_bytes())
         .map_err(|x| format!("Could not write to stdout: {}", x))?;
 
-    handle.flush()
-        .map_err(|x| format!("Could not flush stdout: {}",x))?;
+    handle
+        .flush()
+        .map_err(|x| format!("Could not flush stdout: {}", x))?;
     Ok(())
 }
