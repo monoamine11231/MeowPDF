f88d866b7da447ae8e5712fe9e381b7d3643b0af
monoamine11231 <monoamine11231@gmail.com>
2024-05-15 20:02:45 +0200

    feat(core): Offset states for the PDF viewer were put into a separate struct + primitive working PDF viewer with scrolling capabilities was implemented

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-mod tui;
-use crate::tui::*;
-
-mod graphics;
-use crate::graphics::*;
+mod drivers;
+use drivers::{graphics::terminal_graphics_test_support, tui::*};
 
 mod image;
 use crate::image::*;
@@ -11,13 +8,11 @@ use std::ops::Deref;
 use std::path::Path;
 use std::sync::mpsc::channel;
 use std::time::Duration;
-use std::time::SystemTime;
 use std::{io::stdout, io::Write, sync::Mutex, thread};
 
 use mupdf::Colorspace;
 use mupdf::Matrix;
 use mupdf::Pixmap;
-use mupdf::Rect;
 use mupdf::{document::Document, Page};
 use nix::libc;
 use nix::pty::Winsize;
@@ -29,10 +24,11 @@ use notify::{
     Watcher,
 };
 
-const PRECISION: f64 = 3.0f64;
-const SCROLL: f32 = 1.0f32;
+const PRECISION: f64 = 2.0f64;
+const SCROLL: f32 = 10.0f32;
 const SCALE: f32 = 0.5f32;
 const SCALE_MIN: f32 = 0.2f32;
+const MARGIN_BOTTOM: f32 = 100.0f32;
 
 fn main() {
     /* ============================= Uncook the terminal ============================= */
@@ -107,18 +103,18 @@ fn main() {
     let mut ctm: Matrix = Matrix::new_scale(PRECISION as f32, PRECISION as f32);
     let cs: Colorspace = Colorspace::device_rgb();
 
-    let mut pm: Pixmap = app.cache[0].to_pixmap(&ctm, &cs, 0.0, true).unwrap();
+    let pxpercol: f64 = app.size.ws_xpixel as f64 / app.size.ws_col as f64;
+    let pxperrow: f64 = app.size.ws_ypixel as f64 / app.size.ws_row as f64;
 
-    let b: Rect = app.cache[0].bounds().unwrap();
+    let paddingcol: usize = (pxpercol * PRECISION / SCALE_MIN as f64).ceil() as usize;
+    let paddingrow: usize = (pxperrow * PRECISION / SCALE_MIN as f64).ceil() as usize;
 
-    let a = SystemTime::now();
-    let img: Image = Image::new(&pm, PRECISION).unwrap();
-
-    // panic!("w: {}|h: {}", pm.width(), pm.height());
-    // match a.elapsed() {
-    //     Ok(x) => panic!("aa: {}", x.as_millis()),
-    //     Err(_) => panic!("asdasd"),
-    // };
+    let padding: usize = std::cmp::max(paddingcol, paddingrow);
+    for i in 0..5 {
+        let pm: Pixmap = app.cache[i].to_pixmap(&ctm, &cs, 0.0, true).unwrap();
+        app.images
+            .push(Image::new(&pm, PRECISION, padding).unwrap());
+    }
 
     let mut key_iter = terminal_tui_key_iter().peekable();
     let mut winsize_iter = receive_winsize.try_iter().peekable();
@@ -131,20 +127,6 @@ fn main() {
             let event: (bool, bool) = handle_key(key, &mut app);
             if event.0 {
                 break;
-            }
-
-            if event.1 {
-                ctm = Matrix::new_scale(app.scale, app.scale);
-                pm = app.cache[0].to_pixmap(&ctm, &cs, 0.0, true).unwrap();
-
-                terminal_graphics_transfer_bitmap(
-                    228,
-                    pm.width() as usize,
-                    pm.height() as usize,
-                    pm.samples(),
-                    false,
-                )
-                .unwrap();
             }
 
             key_iter.next();
@@ -163,13 +145,14 @@ fn main() {
 
         terminal_tui_clear().expect("Error when clearing the screen");
 
-        img.display(
-            app.offset.0 as i32,
-            app.offset.1 as i32,
-            app.scale as f64,
-            &app.size,
-        ).unwrap();
+        app.display_pages().expect("Could not display pages");
 
+        print!(
+            "\x1B[{};1H\x1B[1;31mPAGE {} | SCALE {}",
+            app.size.ws_row,
+            app.offset.page(),
+            app.scale
+        );
         stdout().flush().expect("Could not flush stdout.");
 
         while !chan_has!(key_iter) && !chan_has!(winsize_iter) && !chan_has!(file_iter) {
@@ -192,19 +175,19 @@ fn handle_key(key: TerminalKey, app: &mut AppState) -> (bool, bool) {
         | TerminalKey::OTHER(b'q')
         | TerminalKey::OTHER(b'Q') => (true, false),
         TerminalKey::UP => {
-            app.offset.1 -= SCROLL;
+            app.offset.scroll((0.0f32, -SCROLL));
             return (false, false);
         }
         TerminalKey::DOWN => {
-            app.offset.1 += SCROLL;
+            app.offset.scroll((0.0f32, SCROLL));
             return (false, false);
         }
         TerminalKey::LEFT => {
-            app.offset.0 -= SCROLL;
+            app.offset.scroll((-SCROLL, 0.0f32));
             return (false, false);
         }
         TerminalKey::RIGHT => {
-            app.offset.0 += SCROLL;
+            app.offset.scroll((SCROLL, 0.0f32));
             return (false, false);
         }
         TerminalKey::OTHER(b'+') => {
@@ -223,18 +206,89 @@ fn handle_key(key: TerminalKey, app: &mut AppState) -> (bool, bool) {
     return res;
 }
 
+struct ViewerOffset {
+    page: usize,
+    /* Offset is given in page width and page height units */
+    offset: (f32, f32),
+    cumulative_heights: Vec<f32>,
+}
+
+impl ViewerOffset {
+    fn new(cumulative_heights: Vec<f32>) -> Self {
+        Self {
+            page: 0,
+            offset: (0.0f32, 0.0f32),
+            cumulative_heights: cumulative_heights,
+        }
+    }
+
+    fn scroll(&mut self, amount: (f32, f32)) {
+        self.offset.0 += amount.0;
+        self.offset.1 += amount.1;
+
+        /* Update page index by performing binary search */
+        let res = self.cumulative_heights.binary_search_by(|x: &f32| {
+            x.partial_cmp(&self.offset.1)
+                .expect("NaN value found in cumulative height vector")
+        });
+
+        let index: usize = match res {
+            Ok(x) => x,
+            Err(x) => x,
+        };
+
+        self.page = index;
+    }
+
+    fn jump(&mut self, page: usize) {
+        self.page = page;
+
+        if page == 0 {
+            self.offset.1 = 0.0f32;
+        } else {
+            self.offset.1 = self.cumulative_heights[page - 1];
+        }
+    }
+
+    fn page(&self) -> usize {
+        return self.page;
+    }
+
+    fn offset(&self) -> (f32, f32) {
+        return self.offset;
+    }
+
+    /* ============================= Calculation methods ============================= */
+    fn page_height(&self, page: usize) -> Result<f32, String> {
+        let page_prev_height: f32;
+        if page > 0 {
+            page_prev_height = *self.cumulative_heights.get(page - 1).unwrap_or(&0.0f32);
+        } else {
+            page_prev_height = 0.0f32;
+        }
+
+        let page_height: f32;
+        page_height = *self.cumulative_heights.get(page).ok_or(format!(
+            "Wrong page index provided when retrieving page height, index: {}",
+            page
+        ))? - page_prev_height;
+
+        Ok(page_height)
+    }
+}
+
 struct AppState {
     size: Winsize,
     file: String,
     document: Document,
-    cumulative_heights: Vec<f32>,
     cache: Vec<Page>,
+    images: Vec<Image>,
     scale: f32,
-    /* Offset is given in page width and page height units */
-    offset: (f32, f32),
+    offset: ViewerOffset,
 }
 
 impl AppState {
+    /* =============================== Document loading ============================== */
     fn init(file: String) -> Result<Self, String> {
         let winsize: Winsize = terminal_tui_get_dimensions()
             .map_err(|x| format!("Could not get terminal dimensions: {}", x))?;
@@ -263,78 +317,113 @@ impl AppState {
                 .map_err(|x| format!("Could not get bounds for page {}: {}", i, x))?
                 .height();
 
-            cumulative_heights
-                .push(cumulative_heights.last().unwrap_or(&0.0f32) + height);
+            cumulative_heights.push(
+                cumulative_heights.last().unwrap_or(&0.0f32) + height + MARGIN_BOTTOM,
+            );
             cache.push(page);
         }
-
-        let tmp = cache[0].bounds().unwrap();
-        // panic!("aaaa: {} {}", tmp.width(), tmp.height());
 
         let app: AppState = Self {
             size: winsize,
             file: file,
             document: document,
-            cumulative_heights: cumulative_heights,
             cache: cache,
+            images: Vec::new(),
             scale: 0.2,
-            offset: (50.0f32, 50.0f32),
+            offset: ViewerOffset::new(cumulative_heights),
         };
         Ok(app)
     }
 
-    fn document_load(&mut self) -> Result<(), String> {
-        let mut cumulative_heights: Vec<f32> = Vec::new();
-        let mut cache: Vec<Page> = Vec::new();
+    /* Used when watched document has been changed */
+    // fn document_load(&mut self) -> Result<(), String> {
+    //     let mut cumulative_heights: Vec<f32> = Vec::new();
+    //     let mut cache: Vec<Page> = Vec::new();
 
-        let page_count: i32 = self
-            .document
-            .page_count()
-            .map_err(|x| format!("Could not extract the number of pages: {}", x))?;
+    //     let page_count: i32 = self
+    //         .document
+    //         .page_count()
+    //         .map_err(|x| format!("Could not extract the number of pages: {}", x))?;
 
-        for i in 0..page_count {
-            let page: Page = self
-                .document
-                .load_page(i)
-                .map_err(|x| format!("Could not load page {}: {}", i, x))?;
+    //     for i in 0..page_count {
+    //         let page: Page = self
+    //             .document
+    //             .load_page(i)
+    //             .map_err(|x| format!("Could not load page {}: {}", i, x))?;
 
-            let height: f32 = page
-                .bounds()
-                .map_err(|x| format!("Could not get bounds for page {}: {}", i, x))?
-                .height();
+    //         let height: f32 = page
+    //             .bounds()
+    //             .map_err(|x| format!("Could not get bounds for page {}: {}", i, x))?
+    //             .height();
 
-            cumulative_heights
-                .push(cumulative_heights.last().unwrap_or(&0.0f32) + height);
-            cache.push(page);
-        }
+    //         cumulative_heights.push(
+    //             cumulative_heights.last().unwrap_or(&0.0f32) + height + MARGIN_BOTTOM,
+    //         );
+    //         cache.push(page);
+    //     }
 
-        self.cumulative_heights = cumulative_heights;
-        self.cache = cache;
+    //     self.cache = cache;
 
-        Ok(())
-    }
+    //     Ok(())
+    // }
 
-    /* Simple binary search to find page id based on y offset */
-    fn page_index(&self, y_offset: f32) -> Result<usize, String> {
-        if y_offset < 0.0 {
-            Err("Provided y offset is less than 0.0".to_string())?;
-        }
+    /* ================================ Miscellaneous ================================ */
 
-        if self.cumulative_heights.len() == 0 {
-            Err("No pages are loaded")?;
-        }
+    /* Displays the pages based on the internal state of the offset.
+     * Calculates how many pages should be rendered based on the terminal size
+     */
+    fn display_pages(&self) -> Result<(), String> {
+        /* The index of the first rendered page */
+        let mut page_index: usize = self.offset.page();
 
-        let res = self.cumulative_heights.binary_search_by(|x: &f32| {
-            x.partial_cmp(&y_offset)
-                .expect("NaN value found in cumulative height vector")
-        });
-
-        let id: usize = match res {
-            Ok(x) => x,
-            Err(x) => x - 1,
+        /* Offset inside target page */
+        let mut page_offset: f32 = self.offset.offset().1;
+        page_offset -= if page_index == 0 {
+            0.0f32
+        } else {
+            self.offset.cumulative_heights[page_index - 1]
         };
 
-        Ok(id)
+        self.images[page_index]
+            .display(
+                self.offset.offset().0 as i32,
+                (-page_offset * self.scale) as i32,
+                self.scale as f64,
+                &self.size,
+            )
+            .map_err(|x| format!("Could not display page {}: {}", 0, x))?;
+
+        let mut px_displayed_vertically: usize = ((self
+            .offset
+            .page_height(page_index)
+            .map_err(|x| {
+            format!("Could not retrieve page height: {}", x)
+        })? - page_offset)
+            * self.scale) as usize;
+
+        page_index += 1;
+        while px_displayed_vertically < self.size.ws_ypixel as usize {
+            if page_index >= self.images.len() {
+                break;
+            }
+            self.images[page_index]
+                .display(
+                    self.offset.offset().0 as i32,
+                    px_displayed_vertically as i32,
+                    self.scale as f64,
+                    &self.size,
+                )
+                .map_err(|x| format!("Could not display page {}: {}", page_index, x))?;
+
+            px_displayed_vertically += (self
+                .offset
+                .page_height(page_index)
+                .map_err(|x| format!("Could not retrieve page height: {}", x))?
+                * self.scale) as usize;
+            page_index += 1;
+        }
+
+        Ok(())
     }
 }
 
