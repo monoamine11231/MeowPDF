a27a3bcf68ac9401edfbbac7e676b107b92b6f4a
Phil Thompson <34678+PhilT@users.noreply.github.com>
2025-12-20 20:36:02 +0000

    Make inverse_scroll invert mouse only

--- src/threads/event.rs
+++ src/threads/event.rs
@@ -2,21 +2,26 @@ use std::{sync::atomic::Ordering, thread};
 
 use crossbeam_channel::{unbounded, Receiver};
 use crossterm::event::{
-    read, Event, KeyCode, KeyEvent, KeyEventKind, KeyEventState, MouseEvent,
+    read, Event, KeyCode, KeyEvent, KeyEventKind, KeyEventState, KeyModifiers, MouseEvent,
     MouseEventKind,
 };
 
 use crate::{drivers::graphics::GraphicsResponse, globals::RUNNING};
 
+pub enum InputEvent {
+    Key(KeyEvent),
+    MouseScroll(MouseEventKind, KeyModifiers),
+}
+
 pub struct EventThreadData(
-    pub Receiver<KeyEvent>,
+    pub Receiver<InputEvent>,
     pub Receiver<MouseEvent>,
     pub Receiver<GraphicsResponse>,
     pub Receiver<(u16, u16)>,
 );
 
 pub fn spawn() -> EventThreadData {
-    let (sender_key, receive_key) = unbounded::<KeyEvent>();
+    let (sender_input, receive_input) = unbounded::<InputEvent>();
     let (sender_mouse, receive_mouse) = unbounded::<MouseEvent>();
     let (sender_gr, receive_gr) = unbounded::<GraphicsResponse>();
     let (sender_ws, receive_ws) = unbounded::<(u16, u16)>();
@@ -25,8 +30,8 @@ pub fn spawn() -> EventThreadData {
         while RUNNING.load(Ordering::Acquire) {
             match read().expect("Could not read event") {
                 Event::Key(event) => {
-                    sender_key
-                        .try_send(event)
+                    sender_input
+                        .try_send(InputEvent::Key(event))
                         .expect("Could not send key event");
                 }
                 Event::ApplicationProgramCommand(command) => {
@@ -36,63 +41,13 @@ pub fn spawn() -> EventThreadData {
                 }
                 Event::Mouse(event) => match event {
                     MouseEvent {
-                        kind: MouseEventKind::ScrollUp,
+                        kind: kind @ (MouseEventKind::ScrollUp | MouseEventKind::ScrollDown | MouseEventKind::ScrollLeft | MouseEventKind::ScrollRight),
                         modifiers,
                         ..
                     } => {
-                        sender_key
-                            .try_send(KeyEvent {
-                                code: KeyCode::Down,
-                                modifiers,
-                                kind: KeyEventKind::Press,
-                                state: KeyEventState::NONE,
-                            })
-                            .expect("Could not send key event");
-                        sender_mouse.try_send(event).expect("Could not send mouse");
-                    }
-                    MouseEvent {
-                        kind: MouseEventKind::ScrollLeft,
-                        modifiers,
-                        ..
-                    } => {
-                        sender_key
-                            .try_send(KeyEvent {
-                                code: KeyCode::Right,
-                                modifiers,
-                                kind: KeyEventKind::Press,
-                                state: KeyEventState::NONE,
-                            })
-                            .expect("Could not send key event");
-                        sender_mouse.try_send(event).expect("Could not send mouse");
-                    }
-                    MouseEvent {
-                        kind: MouseEventKind::ScrollRight,
-                        modifiers,
-                        ..
-                    } => {
-                        sender_key
-                            .try_send(KeyEvent {
-                                code: KeyCode::Left,
-                                modifiers,
-                                kind: KeyEventKind::Press,
-                                state: KeyEventState::NONE,
-                            })
-                            .expect("Could not send key event");
-                        sender_mouse.try_send(event).expect("Could not send mouse");
-                    }
-                    MouseEvent {
-                        kind: MouseEventKind::ScrollDown,
-                        modifiers,
-                        ..
-                    } => {
-                        sender_key
-                            .try_send(KeyEvent {
-                                code: KeyCode::Up,
-                                modifiers,
-                                kind: KeyEventKind::Press,
-                                state: KeyEventState::NONE,
-                            })
-                            .expect("Could not send key event");
+                        sender_input
+                            .try_send(InputEvent::MouseScroll(kind, modifiers))
+                            .expect("Could not send mouse scroll event");
                         sender_mouse.try_send(event).expect("Could not send mouse");
                     }
                     x => {
@@ -111,5 +66,5 @@ pub fn spawn() -> EventThreadData {
         }
     });
 
-    EventThreadData(receive_key, receive_mouse, receive_gr, receive_ws)
+    EventThreadData(receive_input, receive_mouse, receive_gr, receive_ws)
 }
